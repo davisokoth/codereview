c201642040d891f3aeb867af1d4bb1ff32f19cc3
Davis O <davisokoth@gmail.com>
2019-03-21 20:06:02 +0300

    tests: fix wrong file references
    Fix file switch between `index.test.js` and `index.js` in `TimerTrigger`

--- TimerTrigger/index.js
+++ TimerTrigger/index.js
@@ -1,8 +1,9 @@
-const timerFunction = require('./index');
-const context = require('../testing/defaultContext');
-const timer = require('../testing/defaultTimer');
-
-test('Timer trigger should log message', () => {
-    timerFunction(context, timer);
-    expect(context.log.mock.calls.length).toBe(1);
-});
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
+};
