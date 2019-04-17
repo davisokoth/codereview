b2d3cb8317e862235a746d619d3dec4472a5980a
Davis O <davisokoth@gmail.com>
2019-04-17 16:04:10 +0300

    user: persist profile info

--- storage/users/user.model.ts
+++ storage/users/user.model.ts
@@ -5,19 +5,20 @@ class UserModel {
     login?: string;
     avatar_url?: string;
     url?: string;
-    repos_url?: string;
-    received_events_url?: string;
+    repo_url?: string;
     type?: string;
     score?: number;
     followers?: number;
+    profile: string;
     
     static tableName: string = 'Users';
 
-    constructor(userId: string, name: string, email: string, avatar_url: string) {
+    constructor(userId: string, name: string, email: string, avatar_url: string, profile: string) {
       this.userId = userId;
       this.email = email;
       this.name = name;
       this.avatar_url = avatar_url;
+      this.profile = profile;
     }
 }
   
